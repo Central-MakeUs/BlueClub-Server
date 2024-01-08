15085b72552e7cf686343ac2b691d87550bcc1c0
Ji-Un-Gil <wldns2577@gmail.com>
2024-01-09 01:16:39 +0900

    Feat: Create Login Handler

---
+++ src/main/java/blueclub/server/auth/handler/LoginFailureHandler.java
@@ -0,0 +1,24 @@
+package blueclub.server.auth.handler;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+
+import java.io.IOException;
+
+/**
+ * JWT 로그인 실패 시 처리하는 핸들러
+ * SimpleUrlAuthenticationFailureHandler를 상속받아서 구현
+ */
+public class LoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+                                        AuthenticationException exception) throws IOException, IOException {
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/plain;charset=UTF-8");
+        response.getWriter().write("로그인 실패! 이메일이나 비밀번호를 확인해주세요.");
+    }
+}
