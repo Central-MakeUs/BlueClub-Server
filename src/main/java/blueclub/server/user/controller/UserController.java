6f2a54e14af5360c5a7e0a2479a3f13b3bd8f276
Ji-Un-Gil <wldns2577@gmail.com>
2024-01-09 01:16:58 +0900

    Feat: Create User Service & Controller

---
+++ src/main/java/blueclub/server/user/controller/UserController.java
@@ -0,0 +1,12 @@
+package blueclub.server.user.controller;
+
+import blueclub.server.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+}
---
+++ src/main/java/blueclub/server/user/service/UserService.java
@@ -0,0 +1,14 @@
+package blueclub.server.user.service;
+
+import blueclub.server.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+}
