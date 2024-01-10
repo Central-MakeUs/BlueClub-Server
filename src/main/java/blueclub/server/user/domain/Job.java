82f9ca95ceaa9756f11fef9105981a962db1d810
Ji-Un-Gil <wldns2577@gmail.com>
2024-01-10 21:59:21 +0900

    Feat: Add Job find function

--- src/main/java/blueclub/server/user/domain/Job.java
+++ src/main/java/blueclub/server/user/domain/Job.java
@@ -1,5 +1,7 @@
 package blueclub.server.user.domain;
 
+import blueclub.server.global.response.BaseException;
+import blueclub.server.global.response.BaseResponseStatus;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -10,8 +12,17 @@ public enum Job {
     RIDER("DELIVERY_RIDER", "배달 라이더"),
     DELIVERY("DELIVERY_MAN", "택배 기사"),
     CONSULTANT("FINANCIAL_CONSULTANT", "보험 설계사"),
-    ETC("ETC_FREELANCER", "기타, 프리랜서");
+    ETC("ETC_FREELANCER", "기타·프리랜서");
 
     private final String key;
     private final String title;
+
+    public static Job findByTitle(String title) {
+        for (Job job : Job.values()) {
+            if (job.getTitle().equals(title)) {
+                return job;
+            }
+        }
+        throw new BaseException(BaseResponseStatus.JOB_NOT_FOUND_ERROR);
+    }
 }
