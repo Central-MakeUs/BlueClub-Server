79b0d717cd1da8d585372608a095260c1f85cbde
Ji-Un-Gil <wldns2577@gmail.com>
2024-01-10 22:00:46 +0900

    Feat: Create user find service

---
+++ src/main/java/blueclub/server/user/service/UserFindService.java
@@ -0,0 +1,24 @@
+package blueclub.server.user.service;
+
+import blueclub.server.global.response.BaseException;
+import blueclub.server.global.response.BaseResponseStatus;
+import blueclub.server.user.domain.Email;
+import blueclub.server.user.domain.User;
+import blueclub.server.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class UserFindService {
+
+    private final UserRepository userRepository;
+
+    public User findByUserDetails(UserDetails userDetails) {
+        return userRepository.findByEmail(Email.from(userDetails.getUsername()))
+                .orElseThrow(() -> new BaseException(BaseResponseStatus.MEMBER_NOT_FOUND_ERROR));
+    }
+}
