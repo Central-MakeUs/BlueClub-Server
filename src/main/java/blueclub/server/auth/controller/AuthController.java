032ce61f1defc488cd2f57923a9e6526c8d2bd4c
Ji-Un-Gil <wldns2577@gmail.com>
2024-01-16 06:51:41 +0900

    Feat: Create Login, CheckNickname logic

--- src/main/java/blueclub/server/auth/controller/AuthController.java
+++ src/main/java/blueclub/server/auth/controller/AuthController.java
@@ -1,33 +1,34 @@
 package blueclub.server.auth.controller;
 
-import blueclub.server.auth.domain.SocialType;
+import blueclub.server.auth.dto.request.SocialLoginRequest;
+import blueclub.server.auth.dto.response.SocialLoginResponse;
+import blueclub.server.auth.service.AuthService;
 import blueclub.server.global.response.BaseException;
+import blueclub.server.global.response.BaseResponse;
 import blueclub.server.global.response.BaseResponseStatus;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.net.URI;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/auth")
 public class AuthController {
 
-    @RequestMapping("/login/{socialType}")
-    public ResponseEntity<?> socialLogin(@PathVariable String socialType) {
-        HttpHeaders headers = new HttpHeaders();
-        if (socialType.equals(SocialType.KAKAO.toString().toLowerCase())) {
-            headers.setLocation(URI.create("/oauth2/authorization/kakao"));
-            return new ResponseEntity<>(headers, HttpStatus.MOVED_PERMANENTLY);
-        }
-        if (socialType.equals(SocialType.NAVER.toString().toLowerCase())) {
-            headers.setLocation(URI.create("/oauth2/authorization/naver"));
-            return new ResponseEntity<>(headers, HttpStatus.MOVED_PERMANENTLY);
-        }
-        throw new BaseException(BaseResponseStatus.BAD_GATEWAY);
+    private final AuthService authService;
+
+    @PostMapping("")
+    public ResponseEntity<BaseResponse> socialLogin(@RequestBody SocialLoginRequest socialLoginRequest) {
+        SocialLoginResponse socialLoginResponse = authService.socialLogin(socialLoginRequest);
+        if (socialLoginResponse.role() == null)
+            return BaseResponse.toResponseEntityContainsStatusAndResult(BaseResponseStatus.CREATED, socialLoginResponse);
+        return BaseResponse.toResponseEntityContainsResult(socialLoginResponse);
+    }
+
+    @GetMapping("/duplicated")
+    public ResponseEntity<BaseResponse> checkNickname(@RequestParam("nickname") String nickname) {
+        if (authService.checkNickname(nickname))
+            throw new BaseException(BaseResponseStatus.DUPLICATED_NICKNAME);
+        return BaseResponse.toResponseEntityContainsStatus(BaseResponseStatus.SUCCESS);
     }
 }
--- src/main/java/blueclub/server/auth/service/AuthService.java
+++ src/main/java/blueclub/server/auth/service/AuthService.java
@@ -1,32 +1,102 @@
 package blueclub.server.auth.service;
 
+import blueclub.server.auth.domain.Role;
+import blueclub.server.auth.domain.SocialType;
+import blueclub.server.auth.dto.request.SocialLoginRequest;
+import blueclub.server.auth.dto.response.SocialLoginResponse;
 import blueclub.server.global.response.BaseException;
 import blueclub.server.global.response.BaseResponseStatus;
-import blueclub.server.user.domain.Email;
 import blueclub.server.user.domain.User;
 import blueclub.server.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
-public class AuthService implements UserDetailsService {
+@Transactional
+public class AuthService {
 
+    private final JwtService jwtService;
     private final UserRepository userRepository;
 
-    @Override
-    public UserDetails loadUserByUsername(String email) {
-        User user = userRepository.findByEmail(Email.from(email))
-                .orElseThrow(() -> new BaseException(BaseResponseStatus.MEMBER_NOT_FOUND_ERROR));
-
-        return org.springframework.security.core.userdetails.User.builder()
-                .username(user.getEmail().getValue())
-                // .password(user.getPassword())
-                .roles(user.getRole().name())
+    public SocialLoginResponse socialLogin(SocialLoginRequest socialLoginRequest) {
+        User user = isRegister(socialLoginRequest);
+        String refreshToken = jwtService.createRefreshToken();
+        jwtService.updateRefreshToken(user.getId(), refreshToken);
+        // 유저 정보, accesstoken, refreshtoken 전달 필요
+        if (user.getRole().equals(Role.USER)) {
+            return SocialLoginResponse.builder()
+                    .id(user.getId())
+                    .email(user.getEmail())
+                    .name(user.getName())
+                    .nickname(user.getNickname())
+                    .phoneNumber(user.getPhoneNumber())
+                    .profileImage(user.getProfileImage())
+                    .job(user.getJob().getTitle())
+                    .jobStart(user.getJobStart())
+                    .tosAgree(user.getTosAgree())
+                    .role(user.getRole().getTitle())
+                    .socialType(user.getSocialType().name())
+                    .socialId(user.getSocialId())
+                    .accessToken(jwtService.createAccessToken(user.getEmail()))
+                    .refreshToken(refreshToken)
+                    .build();
+        }
+        return SocialLoginResponse.builder()
+                .id(user.getId())
+                .nickname(user.getNickname())
+                .accessToken(jwtService.createAccessToken(user.getEmail()))
+                .refreshToken(refreshToken)
                 .build();
+    }
+
+    public boolean checkNickname(String nickname) {
+        return userRepository.existsByNickname(nickname).equals(true);
+    }
+
+    // 분기 처리, 유저 정보 반환
+    private User isRegister(SocialLoginRequest socialLoginRequest) {
+        SocialType socialType = SocialType.valueOf(socialLoginRequest.socialType().toUpperCase());
+        Optional<User> user = userRepository.findBySocialTypeAndSocialId(socialType, socialLoginRequest.socialId());
+        // 재로그인
+        if (user.isPresent()) {
+            return user.get();
+        }
+        // 회원가입
+        User newUser = createUser(socialLoginRequest.socialId(), socialType, socialLoginRequest.name(), socialLoginRequest.nickname(),
+                socialLoginRequest.email(), socialLoginRequest.phoneNumber(), socialLoginRequest.profileImage());
+        userRepository.save(newUser);
+        return newUser;
+    }
+
+    private User createUser(String socialId, SocialType socialType, String name, String nickname, String email, String phoneNumber, String profileImage) {
+        // 이메일 중복여부 체크
+        if (userRepository.existsByEmail(email))
+            throw new BaseException(BaseResponseStatus.DUPLICATED_EMAIL);
+        // 분기 처리, 닉네임 중복 시 뒤에 랜덤값 추가
+        if (checkNickname(nickname))
+            nickname = createTemporaryNickname(nickname);
+        return User.builder()
+                .socialId(socialId)
+                .socialType(socialType)
+                .name(name)
+                .nickname(nickname)
+                .email(email)
+                .phoneNumber(phoneNumber)
+                .profileImage(profileImage)
+                .role(Role.GUEST)
+                .build();
+    }
+
+    private String createTemporaryNickname(String nickname) {
+        StringBuilder stringBuilder = new StringBuilder();
+        return stringBuilder
+                .append(nickname)
+                .append("#")
+                .append(RandomStringUtils.randomAlphabetic(3)).toString();
     }
 }
