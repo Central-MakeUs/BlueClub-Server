4f87919fcd9a19fd9486a035c51e666cbe899516
Ji-Un-Gil <wldns2577@gmail.com>
2024-02-02 22:41:23 +0900

    Chore: Add dto builder pattern

--- src/main/java/blueclub/server/notice/dto/response/GetNoticeListResponse.java
+++ src/main/java/blueclub/server/notice/dto/response/GetNoticeListResponse.java
@@ -1,9 +1,11 @@
 package blueclub.server.notice.dto.response;
 
 import com.querydsl.core.annotations.QueryProjection;
+import lombok.Builder;
 
 import java.time.LocalDateTime;
 
+@Builder
 public record GetNoticeListResponse (
         Long id,
         String title,
--- src/main/java/blueclub/server/reminder/dto/response/GetReminderListResponse.java
+++ src/main/java/blueclub/server/reminder/dto/response/GetReminderListResponse.java
@@ -1,9 +1,11 @@
 package blueclub.server.reminder.dto.response;
 
 import com.querydsl.core.annotations.QueryProjection;
+import lombok.Builder;
 
 import java.time.LocalDateTime;
 
+@Builder
 public record GetReminderListResponse (
         Long id,
         String title,
