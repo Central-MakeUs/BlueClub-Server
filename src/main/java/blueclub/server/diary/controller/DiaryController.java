222d5d022c5b0c31eec1da5dbd5cb9e23d7438a4
Ji-Un-Gil <wldns2577@gmail.com>
2024-02-08 06:38:24 +0900

    Feat: Add Diary crud logic about dayoff case

--- src/main/java/blueclub/server/diary/controller/DiaryController.java
+++ src/main/java/blueclub/server/diary/controller/DiaryController.java
@@ -1,9 +1,7 @@
 package blueclub.server.diary.controller;
 
-import blueclub.server.diary.dto.request.UpdateCaddyDiaryRequest;
-import blueclub.server.diary.dto.request.UpdateDayworkerDiaryRequest;
-import blueclub.server.diary.dto.request.UpdateDiaryRequest;
-import blueclub.server.diary.dto.request.UpdateRiderDiaryRequest;
+import blueclub.server.diary.domain.Worktype;
+import blueclub.server.diary.dto.request.*;
 import blueclub.server.diary.service.DiaryService;
 import blueclub.server.global.response.BaseException;
 import blueclub.server.global.response.BaseResponse;
@@ -14,6 +12,7 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.validation.annotation.Validated;
@@ -23,6 +22,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.time.YearMonth;
 import java.util.List;
 
+@PreAuthorize("hasRole('USER')")
 @Validated
 @RestController
 @RequiredArgsConstructor
@@ -35,15 +35,19 @@ public class DiaryController {
     public ResponseEntity<BaseResponse> saveDiary(
             @AuthenticationPrincipal UserDetails userDetails,
             @NotBlank(message = "직업명은 필수입니다") @RequestParam("job") String jobTitle,
-            @Valid @RequestPart("dto") UpdateDiaryRequest updateDiaryRequest,
+            @Valid @RequestPart("dto") UpdateBaseDiaryRequest updateBaseDiaryRequest,
             @RequestPart(value = "image", required = false) List<MultipartFile> multipartFileList
     ) {
-        if (Job.CADDY.getTitle().equals(jobTitle.replace(" ", "")) && updateDiaryRequest instanceof UpdateCaddyDiaryRequest) {
-            return BaseResponse.toResponseEntityContainsStatusAndResult(BaseResponseStatus.CREATED, diaryService.saveCaddyDiary(userDetails, (UpdateCaddyDiaryRequest) updateDiaryRequest, multipartFileList));
-        } else if (Job.RIDER.getTitle().equals(jobTitle.replace(" ", "")) && updateDiaryRequest instanceof UpdateRiderDiaryRequest) {
-            return BaseResponse.toResponseEntityContainsStatusAndResult(BaseResponseStatus.CREATED, diaryService.saveRiderDiary(userDetails, (UpdateRiderDiaryRequest) updateDiaryRequest, multipartFileList));
-        } else if (Job.DAYWORKER.getTitle().equals(jobTitle.replace(" ", "")) && updateDiaryRequest instanceof UpdateDayworkerDiaryRequest) {
-            return BaseResponse.toResponseEntityContainsStatusAndResult(BaseResponseStatus.CREATED, diaryService.saveDayworkerDiary(userDetails, (UpdateDayworkerDiaryRequest) updateDiaryRequest, multipartFileList));
+        if (updateBaseDiaryRequest.getWorktype().equals(Worktype.DAY_OFF.getKey())) {
+            diaryService.saveDayOffDiary(userDetails, updateBaseDiaryRequest);
+            return BaseResponse.toResponseEntityContainsStatus(BaseResponseStatus.CREATED);
+        }
+        if (Job.CADDY.getTitle().equals(jobTitle.replace(" ", "")) && updateBaseDiaryRequest instanceof UpdateCaddyDiaryRequest) {
+            return BaseResponse.toResponseEntityContainsStatusAndResult(BaseResponseStatus.CREATED, diaryService.saveCaddyDiary(userDetails, (UpdateCaddyDiaryRequest) updateBaseDiaryRequest, multipartFileList));
+        } else if (Job.RIDER.getTitle().equals(jobTitle.replace(" ", "")) && updateBaseDiaryRequest instanceof UpdateRiderDiaryRequest) {
+            return BaseResponse.toResponseEntityContainsStatusAndResult(BaseResponseStatus.CREATED, diaryService.saveRiderDiary(userDetails, (UpdateRiderDiaryRequest) updateBaseDiaryRequest, multipartFileList));
+        } else if (Job.DAYWORKER.getTitle().equals(jobTitle.replace(" ", "")) && updateBaseDiaryRequest instanceof UpdateDayworkerDiaryRequest) {
+            return BaseResponse.toResponseEntityContainsStatusAndResult(BaseResponseStatus.CREATED, diaryService.saveDayworkerDiary(userDetails, (UpdateDayworkerDiaryRequest) updateBaseDiaryRequest, multipartFileList));
         } else throw new BaseException(BaseResponseStatus.INVALID_INPUT_DTO);
     }
 
@@ -52,15 +56,19 @@ public class DiaryController {
             @AuthenticationPrincipal UserDetails userDetails,
             @NotBlank(message = "직업명은 필수입니다") @RequestParam("job") String jobTitle,
             @PathVariable("diaryId") Long diaryId,
-            @Valid @RequestPart("dto") UpdateDiaryRequest updateDiaryRequest,
+            @Valid @RequestPart("dto") UpdateBaseDiaryRequest updateBaseDiaryRequest,
             @RequestPart(value = "image", required = false) List<MultipartFile> multipartFileList
     ) {
-        if (Job.CADDY.getTitle().equals(jobTitle.replace(" ", "")) && updateDiaryRequest instanceof UpdateCaddyDiaryRequest) {
-            return BaseResponse.toResponseEntityContainsResult(diaryService.updateCaddyDiary(userDetails, diaryId, (UpdateCaddyDiaryRequest) updateDiaryRequest, multipartFileList));
-        } else if (Job.RIDER.getTitle().equals(jobTitle.replace(" ", "")) && updateDiaryRequest instanceof UpdateRiderDiaryRequest) {
-            return BaseResponse.toResponseEntityContainsResult(diaryService.updateRiderDiary(userDetails, diaryId, (UpdateRiderDiaryRequest) updateDiaryRequest, multipartFileList));
-        } else if (Job.DAYWORKER.getTitle().equals(jobTitle.replace(" ", "")) && updateDiaryRequest instanceof UpdateDayworkerDiaryRequest) {
-            return BaseResponse.toResponseEntityContainsResult(diaryService.updateDayworkerDiary(userDetails, diaryId, (UpdateDayworkerDiaryRequest) updateDiaryRequest, multipartFileList));
+        if (updateBaseDiaryRequest.getWorktype().equals(Worktype.DAY_OFF.getKey())) {
+            diaryService.updateDayOffDiary(userDetails, diaryId);
+            return BaseResponse.toResponseEntityContainsStatus(BaseResponseStatus.SUCCESS);
+        }
+        if (Job.CADDY.getTitle().equals(jobTitle.replace(" ", "")) && updateBaseDiaryRequest instanceof UpdateCaddyDiaryRequest) {
+            return BaseResponse.toResponseEntityContainsResult(diaryService.updateCaddyDiary(userDetails, diaryId, (UpdateCaddyDiaryRequest) updateBaseDiaryRequest, multipartFileList));
+        } else if (Job.RIDER.getTitle().equals(jobTitle.replace(" ", "")) && updateBaseDiaryRequest instanceof UpdateRiderDiaryRequest) {
+            return BaseResponse.toResponseEntityContainsResult(diaryService.updateRiderDiary(userDetails, diaryId, (UpdateRiderDiaryRequest) updateBaseDiaryRequest, multipartFileList));
+        } else if (Job.DAYWORKER.getTitle().equals(jobTitle.replace(" ", "")) && updateBaseDiaryRequest instanceof UpdateDayworkerDiaryRequest) {
+            return BaseResponse.toResponseEntityContainsResult(diaryService.updateDayworkerDiary(userDetails, diaryId, (UpdateDayworkerDiaryRequest) updateBaseDiaryRequest, multipartFileList));
         } else throw new BaseException(BaseResponseStatus.INVALID_INPUT_DTO);
     }
 
