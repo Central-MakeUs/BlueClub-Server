336410ec95648259960ef607aaea4f0991f702a9
Ji-Un-Gil <wldns2577@gmail.com>
2024-02-17 12:27:30 +0900

    Fix: Add missing where query condition

--- src/main/java/blueclub/server/monthlyGoal/repository/MonthlyGoalQueryRepository.java
+++ src/main/java/blueclub/server/monthlyGoal/repository/MonthlyGoalQueryRepository.java
@@ -2,6 +2,8 @@ package blueclub.server.monthlyGoal.repository;
 
 import blueclub.server.user.domain.User;
 
+import java.time.YearMonth;
+
 public interface MonthlyGoalQueryRepository {
-    Long getRecentMonthlyGoal(User user);
+    Long getRecentMonthlyGoal(User user, YearMonth yearMonth);
 }
