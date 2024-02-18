6b6dec82fb65b70979b1fbd1dd93968ffc9563b0
Ji-Un-Gil <wldns2577@gmail.com>
2024-02-18 21:36:05 +0900

    Test: Update rate limit and related test code

--- src/test/java/blueclub/server/auth/config/TestRateLimitingInterceptor.java
+++ src/test/java/blueclub/server/auth/config/TestRateLimitingInterceptor.java
@@ -34,8 +34,8 @@ public class TestRateLimitingInterceptor implements HandlerInterceptor {
     }
 
     private Bucket createBucket() {
-        Refill refill = Refill.intervally(1, Duration.ofMillis(10)); // 1밀리초당 1회 충전
-        Bandwidth limit = Bandwidth.classic(1, refill); // 최대 1회 가능
+        Refill refill = Refill.intervally(10, Duration.ofMillis(100)); // 0.1초당 10회 충전
+        Bandwidth limit = Bandwidth.classic(10, refill); // 최대 10회 가능
         return Bucket.builder()
                 .addLimit(limit)
                 .build();
